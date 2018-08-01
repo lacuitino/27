483e58435ce8ddd9054e577aef8efa0bbd53a6ec
Lucas Cuitino <cuitino.lu@gmail.com>
2018-07-31 21:56:09 -0400

    This is a base (non final edition) of the form in the Show view.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,10 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:name, :price)
   end
+
+  def create
+    @product = Products.new(product_params)
+    @product.save
+    redirect_to category_path(@product.category_id)
+  end
 end
